bc416ff28db7287fb042edf53af1a1bff630baba
Bagas2512 <85236373+Bagas2512@users.noreply.github.com>
2021-06-02 20:24:16 +0700

    Add files via upload

---
+++ Modul_02_ifElse.php
@@ -0,0 +1,12 @@
+<?php
+    $d = 10; // ganti nilai d dan perhatikan perubahan
+    if ($d == 10) // menggunakan operator pembanding ==
+
+    {
+        echo "benar nilai d sama dengan 10";
+
+    }else
+    {
+        echo "salah, nilai d bukan 10";
+    }
+?>
